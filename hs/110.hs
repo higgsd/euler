56570d3be646c4fb0ff05eb518074e0bf8f65b85
David Higgs <higgsd@users.noreply.github.com>
2014-08-31 11:50:58 -0400

    Add solution for 110.

---
+++ hs/110.hs
@@ -0,0 +1,21 @@
+-- 9350130049860600
+import Data.List(group)
+import Euler(primeSieve, primeFactors)
+
+nn = 4000000
+kk = head [n | n <- [1..], 3^n > nn]
+primes = take kk $ primeSieve (nn^2)
+
+genSigmas [] = []
+genSigmas (n:ns)
+    | any (> maximum primes) fs = genSigmas ns
+    | otherwise = (x,n) : genSigmas ns
+    where fs = primeFactors n primes
+          as = map length $ group fs
+          x = product $ map (\a -> 2*a+1) as
+
+findSolution = snd $ head $ dropWhile isSolution $ genSigmas candidates
+    where isSolution x = fst x <= nn * 2
+          candidates = map (\x -> x * (product $ take (kk-2) primes)) [1..]
+
+main = putStrLn $ show $ findSolution
