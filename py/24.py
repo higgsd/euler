cd123204c3384bd3847b34452fc125a7e3a6bf47
David Higgs <higgsd@users.noreply.github.com>
2014-08-31 11:38:32 -0400

    Remove excess whitespace.

--- py/24.py
+++ py/24.py
@@ -2,7 +2,7 @@
 import euler
 N = 10
 T = 1000000
-f = euler.product(xrange(1, N)) 
+f = euler.product(xrange(1, N))
 v = T - 1
 a = []
 d = [n for n in xrange(N)]
@@ -14,4 +14,3 @@ for n in xrange(N):
     if n + 1 != N:
         f /= N - n - 1
 print ''.join([str(x) for x in a])
-
